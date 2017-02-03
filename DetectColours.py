f14f3bcc2ccf10c68f89de2477e4545eca558ebb
Andrea McIntosh <akmcinto@ualberta.ca>
2017-02-03 14:52:14 -0700

    Finding nodes, not ordered

--- DetectColours.py
+++ DetectColours.py
@@ -3,6 +3,8 @@
 import numpy as np
 import cv2
 
+SIG_LENGTH = 10
+
 # load the image
 image = cv2.imread("./paintmaze_small.jpg")
 
@@ -19,33 +21,48 @@ for (lower, upper) in boundaries:
     mask = cv2.inRange(image, lower, upper)
     output = cv2.bitwise_and(image, image, mask = mask)
 
-    # # http://docs.opencv.org/3.2.0/d4/d73/tutorial_py_contours_begin.html, 2017-02-02
-    # im2, contours, heiarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(image, contours, -1, (255, 0, 0), 3)
-
     # http://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/, 2017-02-02
     edges = cv2.Canny(image, 225, 250) # element of edges represents pixels in a row (width)
 
+    # Dicts to hold starting pixels and end pixels of corners in rows
     starts = {}
     ends = {}
     for row_i in range(0, len(edges)):
-        if 255 in edges[row_i]:
+        if 255 in edges[row_i]: # If there are any detected edge pixels
             pix_i = 0
+            # iterate through all pixels in the row
             while pix_i < len(edges[row_i]):
                 start = pix_i
+                # Move along an edge in the row, saving the start of the edge
                 while (edges[row_i][pix_i] == 255):
                     pix_i += 1
-                if pix_i > (start + 10):
+                # If the edge is of significant size (not noise)
+                if pix_i > (start + SIG_LENGTH):
+                    # Make a list of starts and ends of significant edges in row
                     if row_i not in starts:
                         starts[row_i] = []
                         ends[row_i] = []
                     starts[row_i].append(start)
                     ends[row_i].append(pix_i)
                 pix_i += 1
-            # break
 
-    print starts, ends
+    # for k in starts.keys():
+    #     print str(k) + ": "
+    #     print starts[k]
+    #     print ends[k]
 
-    # # # show the images
-    cv2.imshow("images", np.hstack([image[:,:,1], edges]))
+    midlines = []
+    # Compute midpoints of each edge, and save pixel indices as tuples
+    for k in starts.keys():
+        for ind in range(0, len(starts[k])):
+            midpoint = int((ends[k][ind] + starts[k][ind]) / 2)
+            midlines.append((midpoint, k))
+
+    print midlines
+
+    for ind in range(0, len(midlines) - 1):
+        cv2.line(image, midlines[ind], midlines[ind+1], (255, 0, 0))
+
+    # show the images
+    cv2.imshow("images", np.hstack([image]))
     cv2.waitKey(0)
