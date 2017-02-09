8995fef1e8a191cc5684e31d5c10d68220da1c88
Andrea McIntosh <akmcinto@ualberta.ca>
2017-02-08 18:04:48 -0700

    Fixed div coordinates error, have different x/y correctness

--- MazeNodes.py
+++ MazeNodes.py
@@ -1,15 +1,21 @@
-# http://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/, 2017-01-31
 # import the necessary packages
 import numpy as np
 import cv2
+from Node import *
 
 SIG_LENGTH = 10
 
-NUM_DIVS = 32
+NUM_DIVS_X = 64
+NUM_DIVS_Y = 16
 
 # load the image
 image = cv2.imread("./paintmaze_small.jpg")
 
+# Find pixel length of each grid div
+X_DIV_LEN = int(len(image[0]) / NUM_DIVS_X) # floors number of divisions in width
+Y_DIV_LEN = int(len(image) / NUM_DIVS_Y) # floors number of divisions in height
+
+# http://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/, 2017-01-31
 # define the list of colour ranges
 boundaries =  [([240, 240, 240], [255, 255, 255])] # white
 
@@ -27,22 +33,22 @@ for (lower, upper) in boundaries:
     gray_image = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
 
     nodes = []
-    x_div = int(len(gray_image[0]) / NUM_DIVS) # floors number of divisions in width
-    y_div = int(len(gray_image) / NUM_DIVS) # floors number of divisions in height
     # Run through all divisions
-    for div_x in range(0, NUM_DIVS):
-        for div_y in range(0, NUM_DIVS):
+    for div_x in range(0, NUM_DIVS_X):
+        for div_y in range(0, NUM_DIVS_Y):
             wall = False
             # Because image is a list of lists, need to run through the rows for each div
-            for y_i in range(div_y*y_div, (div_y+1)*y_div):
-                if 0 in gray_image[y_i][div_x*x_div:(div_x+1)*x_div]:
+            for y_i in range(div_y*Y_DIV_LEN, (div_y+1)*Y_DIV_LEN):
+                if 0 in gray_image[y_i][div_x*X_DIV_LEN:(div_x+1)*X_DIV_LEN]:
                     wall = True
             # If the div does not contain any wall pixels, add to list of nodes for Dijksta's
             if not wall:
-                nodes.append((x_div*div_x+(div_x/2), y_div*div_y+(div_y/2)))
+                n = Node()
+                n.coordinates = (X_DIV_LEN*div_x+(X_DIV_LEN/2), Y_DIV_LEN*div_y+(Y_DIV_LEN/2))
+                nodes.append(n)
 
     for n in nodes:
-        cv2.circle(image, n, 5, (255, 255, 0), -1)
+        cv2.circle(image, n.coordinates, 5, (255, 255, 0), -1)
 
     # show the images
     cv2.imshow("images", np.hstack([image]))
