e972d64c893b97d7c2ddf757c30da5f1fd66bfed
Andrea McIntosh <akmcinto@ualberta.ca>
2017-02-08 21:36:49 -0700

    Split into functions, takes filename as arg

--- MazeNodes.py
+++ MazeNodes.py
@@ -2,28 +2,36 @@
 import numpy as np
 import cv2
 from Node import *
+import sys
 
 SIG_LENGTH = 10
 
 NUM_DIVS_X = 30
 NUM_DIVS_Y = 20
 
-# load the image
-image = cv2.imread("./paintmaze_small.jpg")
-
-# Find pixel length of each grid div
-X_DIV_LEN = int(len(image[0]) / NUM_DIVS_X) # floors number of divisions in width
-Y_DIV_LEN = int(len(image) / NUM_DIVS_Y) # floors number of divisions in height
-
-# http://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/, 2017-01-31
 # define the list of colour ranges
-boundaries =  [([240, 240, 240], [255, 255, 255])] # white
+WHITE_THRESHOLD =  ([240, 240, 240], [255, 255, 255])
 
-# loop over the boundaries
-for (lower, upper) in boundaries:
+nodes = {}
+
+def main(fn):
+    image, gray_image, x_div_len, y_div_len = getImage(fn)
+    findNodes(gray_image, x_div_len, y_div_len)
+    findEdges(x_div_len, y_div_len)
+    drawResults(image)
+
+def getImage(filename):
+    # load the image
+    image = cv2.imread(filename)
+
+    # Find pixel length of each grid div
+    x_div_len = int(len(image[0]) / NUM_DIVS_X) # floors number of divisions in width
+    y_div_len = int(len(image) / NUM_DIVS_Y) # floors number of divisions in height
+
+    # http://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/, 2017-01-31
     # create NumPy arrays from the boundaries
-    lower = np.array(lower, dtype = "uint8")
-    upper = np.array(upper, dtype = "uint8")
+    lower = np.array(WHITE_THRESHOLD[0], dtype = "uint8")
+    upper = np.array(WHITE_THRESHOLD[1], dtype = "uint8")
 
     mask = cv2.inRange(image, lower, upper)
     output = cv2.bitwise_and(image, image, mask = mask)
@@ -32,25 +40,25 @@ for (lower, upper) in boundaries:
     # http://docs.opencv.org/3.2.0/df/d9d/tutorial_py_colorspaces.html, 2017-02-05
     gray_image = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
 
-    nodes = {}
+    return image, gray_image, x_div_len, y_div_len
+
+def findNodes(gray_image, x_div_len, y_div_len):
     # Run through all divisions
     for div_x in range(0, NUM_DIVS_X):
         for div_y in range(0, NUM_DIVS_Y):
             wall = False
             # Because image is a list of lists, need to run through the rows for each div
-            for y_i in range(div_y*Y_DIV_LEN, (div_y+1)*Y_DIV_LEN):
-                if 0 in gray_image[y_i][div_x*X_DIV_LEN:(div_x+1)*X_DIV_LEN]:
+            for y_i in range(div_y*y_div_len, (div_y+1)*y_div_len):
+                if 0 in gray_image[y_i][div_x*x_div_len:(div_x+1)*x_div_len]:
                     wall = True
             # If the div does not contain any wall pixels, add to list of nodes for Dijksta's
             if not wall:
                 n = Node()
-                n.coordinates = (X_DIV_LEN*div_x+(X_DIV_LEN/2), Y_DIV_LEN*div_y+(Y_DIV_LEN/2))
+                n.coordinates = (x_div_len*div_x+(x_div_len/2), y_div_len*div_y+(y_div_len/2))
                 n.neighbours = [] # need to make sure neighbours is empty
                 nodes[n.coordinates] = n
 
-    for n in nodes:
-        cv2.circle(image, n, 5, (150, 150, 150), -1)
-
+def findEdges(x_div_len, y_div_len):
     # Create edges
     for nc in nodes:
         x = nc[0]
@@ -58,17 +66,22 @@ for (lower, upper) in boundaries:
         nc_neighbours = nodes.get(nc).neighbours
 
         # Check for adjacent nodes in all directions
-        if (x - X_DIV_LEN, y) in nodes:
-            nc_neighbours.append(nodes.get((x - X_DIV_LEN, y)))
+        if (x - x_div_len, y) in nodes:
+            nc_neighbours.append(nodes.get((x - x_div_len, y)))
 
-        if (x + X_DIV_LEN, y) in nodes:
-            nc_neighbours.append(nodes.get((x + X_DIV_LEN, y)))
+        if (x + x_div_len, y) in nodes:
+            nc_neighbours.append(nodes.get((x + x_div_len, y)))
 
-        if (x, y - Y_DIV_LEN) in nodes:
-            nc_neighbours.append(nodes.get((x, y - Y_DIV_LEN)))
+        if (x, y - y_div_len) in nodes:
+            nc_neighbours.append(nodes.get((x, y - y_div_len)))
 
-        if (x, y + Y_DIV_LEN) in nodes:
-            nc_neighbours.append(nodes.get((x, y + Y_DIV_LEN)))
+        if (x, y + y_div_len) in nodes:
+            nc_neighbours.append(nodes.get((x, y + y_div_len)))
+
+def drawResults(image):
+    # Draw nodes
+    for n in nodes:
+        cv2.circle(image, n, 5, (150, 150, 150), -1)
 
     # Draw edges
     for n in nodes:
@@ -82,4 +95,6 @@ for (lower, upper) in boundaries:
     cv2.imshow("images", np.hstack([image]))
     cv2.waitKey(0)
 
-    # cv2.imwrite("maze_points.png", image)
+if __name__ == '__main__':
+    image_name = sys.argv[1]
+    main(image_name)
