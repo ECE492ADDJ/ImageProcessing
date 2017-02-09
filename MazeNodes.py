c43888907f9c082ac8a7cb413178ccd8f12016d4
Andrea McIntosh <akmcinto@ualberta.ca>
2017-02-08 20:32:36 -0700

    Finding edges

--- MazeNodes.py
+++ MazeNodes.py
@@ -5,8 +5,8 @@ from Node import *
 
 SIG_LENGTH = 10
 
-NUM_DIVS_X = 64
-NUM_DIVS_Y = 16
+NUM_DIVS_X = 30
+NUM_DIVS_Y = 20
 
 # load the image
 image = cv2.imread("./paintmaze_small.jpg")
@@ -32,7 +32,7 @@ for (lower, upper) in boundaries:
     # http://docs.opencv.org/3.2.0/df/d9d/tutorial_py_colorspaces.html, 2017-02-05
     gray_image = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
 
-    nodes = []
+    nodes = {}
     # Run through all divisions
     for div_x in range(0, NUM_DIVS_X):
         for div_y in range(0, NUM_DIVS_Y):
@@ -45,10 +45,38 @@ for (lower, upper) in boundaries:
             if not wall:
                 n = Node()
                 n.coordinates = (X_DIV_LEN*div_x+(X_DIV_LEN/2), Y_DIV_LEN*div_y+(Y_DIV_LEN/2))
-                nodes.append(n)
+                n.neighbours = [] # need to make sure neighbours is empty
+                nodes[n.coordinates] = n
 
     for n in nodes:
-        cv2.circle(image, n.coordinates, 5, (255, 255, 0), -1)
+        cv2.circle(image, n, 5, (150, 150, 150), -1)
+
+    # Create edges
+    for nc in nodes:
+        x = nc[0]
+        y = nc[1]
+        nc_neighbours = nodes.get(nc).neighbours
+
+        # Check for adjacent nodes in all directions
+        if (x - X_DIV_LEN, y) in nodes:
+            nc_neighbours.append(nodes.get((x - X_DIV_LEN, y)))
+
+        if (x + X_DIV_LEN, y) in nodes:
+            nc_neighbours.append(nodes.get((x + X_DIV_LEN, y)))
+
+        if (x, y - Y_DIV_LEN) in nodes:
+            nc_neighbours.append(nodes.get((x, y - Y_DIV_LEN)))
+
+        if (x, y + Y_DIV_LEN) in nodes:
+            nc_neighbours.append(nodes.get((x, y + Y_DIV_LEN)))
+
+    # Draw edges
+    for n in nodes:
+        if len(nodes.get(n).neighbours) > 4:
+            print 'Too many neighbours!!'
+        for nb in nodes.get(n).neighbours:
+            cv2.line(image, n, nb.coordinates, (nb.coordinates[0] % 255,
+                nb.coordinates[1] % 255, (nb.coordinates[0] + nb.coordinates[1]) % 255), 3)
 
     # show the images
     cv2.imshow("images", np.hstack([image]))
