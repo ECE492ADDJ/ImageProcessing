ada15e2a6d623d16ea914846b1c6f0b03529a80d
JacobChar <jj.charlebois@gmail.com>
2017-03-06 13:29:54 -0700

    Node.py tests

--- Node.py
+++ Node.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python
+"""
+Node class tests
+>>> A = Node()
+>>> A.neighbours = [1, 2, 3]
+>>> A.start = True
+>>> A.coordinates = (2, 2)
+>>> A.neighbours
+[1, 2, 3]
+>>> A.start 
+True
+>>> A.coordinates
+(2, 2)
+"""
+
 class Node:
 	'Node object which will contain information about intersections in the maze. Each node will maintain whether it is a start or end point, coordinates, and a list of neighbouring nodes.'
 
