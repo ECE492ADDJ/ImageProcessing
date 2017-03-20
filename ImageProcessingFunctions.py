84839f204221da3d852654ea168aeaa2a8e7c287
Andrea McIntosh <akmcinto@ualberta.ca>
2017-03-20 16:32:34 -0600

    Created test function to determine processing frame rate

--- ImageProcessingFunctions.py
+++ ImageProcessingFunctions.py
@@ -97,8 +97,8 @@ def findRegionCenter(mask, filt_close, filt_open):
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, filt_open)
 
     # http://docs.opencv.org/3.1.0/dd/d49/tutorial_py_contour_features.html, 2017-02-08
-    ret, thresh = cv2.threshold(mask, 127, 255, 0)
-    contours = cv2.findContours(thresh, 1, 2)
+    # ret, thresh = cv2.threshold(mask, 127, 255, 0)
+    contours = cv2.findContours(mask, 1, 2)
 
     cnt = contours[0]
     M = cv2.moments(cnt)
