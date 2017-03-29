8b86f406161e50e4ef6a287e3fd18606ebf01e32
JacobChar <jj.charlebois@gmail.com>
2017-03-29 14:55:08 -0600

    Gui commented

--- AutoTiltingBallMaze.py
+++ AutoTiltingBallMaze.py
@@ -25,6 +25,8 @@ import time
 class AutoTiltingBallMaze:
 
 	def __init__(self):
+
+		# Main Tkinter window that we add our elements too
 		root = Tk()
 		root.title("Auto Tilting Ball Maze")
 		root.configure(bg="#007777")
@@ -32,7 +34,7 @@ class AutoTiltingBallMaze:
 		# Make fullscreen
 		root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
-		# Labels
+		# Labels such as our thresholds, camera index, and serial port
 		Label(root, text="Please Enter Variables", font=("Helvetica", 18), bg="#007777").grid(row=0, columnspan=3)
 		Label(root, text="Upper (as B,G,R)", font=("Helvetica", 18), bg="#007777").grid(row=1, column=1)
 		Label(root, text="Lower (as B,G,R)", font=("Helvetica", 18), bg="#007777").grid(row=1, column=2)
@@ -43,7 +45,7 @@ class AutoTiltingBallMaze:
 		Label(root, text="Camera",font=("Helvetica", 16), bg="#007777").grid(row=6, sticky=E)
 		Label(root, text="  ", bg="#007777").grid(row=7, sticky=E)
 
-		# Text Fields
+		# Text Fields with defaulted thresholds to save ourselves time
 		self.playSpaceEntry1 = Entry(root)
 		self.playSpaceEntry1.insert(END, "255,255,255")
 		self.playSpaceEntry1.grid(row=2,column=1)
@@ -138,8 +140,10 @@ class AutoTiltingBallMaze:
 		self.lmain.after(10, self.show_frame) 
 
 
+	# Grabs the variables to be input into our MazeSolver class
 	def grabVariables(self):
 
+		# Grab all the variables
 		playSpaceUpper = self.playSpaceEntry1.get()
 		playSpaceLower = self.playSpaceEntry2.get()
 		startUpper = self.startEntry1.get()
@@ -157,8 +161,10 @@ class AutoTiltingBallMaze:
 		endUpper = self.parseThreshold(endUpper)
 		endLower = self.parseThreshold(endLower)
 
+		# Create our MazeSolver object
 		solver = MazeSolver()
 
+		# Set the values for our MazeSolver object
 		# The following values have defaults if not set
 		solver.play_colour_lower = playSpaceLower
 		solver.play_colour_upper = playSpaceUpper
@@ -169,16 +175,18 @@ class AutoTiltingBallMaze:
 		# solver.serial_port = serialPort
 		solver.camera_index = int(camIndex)
 
+		# Release the camera
 		self.cap.release()
 
+		# Run the MazeSolver
 		solver.run()
 
-
+	# Simple function that returns a list of the available serial ports
 	def serial_ports(self):  
 		ports = list(serial.tools.list_ports.comports())
 		return ports
 
-
+	# Simple function that returns the number of cameras connected to the 'server'
 	def detectNumCameras(self):
 		ind = 0
 		# Iterates through indexes until we cant find a camera
@@ -191,7 +199,9 @@ class AutoTiltingBallMaze:
 		    	break
 		return ind
 
-
+	# Turns on the camera and continues to run show_frame() to provide a video feed
+	# for our Gui. If the camera is already in use it is first released so that our set camera
+	# index can be read from instead
 	def checkCamera(self):
 
 		try:
@@ -214,4 +224,5 @@ class AutoTiltingBallMaze:
 		strings = values.split(",")
 		return [int(string) for string in strings]
 
+# Self call to run when compiled
 AutoTiltingBallMaze()
